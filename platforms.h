4d5fc6d602aeb399f3582066b35ada8cf202f91c
mronzon <104356392+mronzon@users.noreply.github.com>
2022-11-29 10:11:58 -0500

    Almost work

--- platforms.h
+++ platforms.h
@@ -1,17 +1,9 @@
 #pragma once
 
-typedef enum
-{
-	horizontal, vertical
-} direction;
-
-
-struct platform_t
+struct position_t
 {
 	int x;
 	int y;
-	int height;
-	int width;
 };
 
 struct moving_lift_t
@@ -20,22 +12,24 @@ struct moving_lift_t
 	int y;
 	int width;
 	int height;
-	direction axe;
-	int limit[2];
-	int initial_velocity;
+	int axe; // 0 for vertical and 1 for horizontal
+	int first_limit;
+	int second_limit;
+	int initial_velocity; // -1 for bottom/left and 1 for up/right
 };
 
 struct balanced_lift_t
 {
 	int x_first;
 	int y_first;
-	int x_second;
-	int y_second;
 	int width_first;
 	int height_first;
+	int x_second;
+	int y_second;
 	int width_second;
 	int height_second;
 	int total_length;
+	int max_x;
 };
 
 // TODO Finish the struct.
