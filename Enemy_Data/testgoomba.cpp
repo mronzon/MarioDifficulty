89e70c6a2900846c86f867b6ff851d592b11c0ee
JulesLinard <79532003+JulesLinard@users.noreply.github.com>
2023-03-14 02:46:45 +0100

    Rajout des déplacements des koopas !

--- Enemy_Data/testgoomba.cpp
+++ Enemy_Data/testgoomba.cpp
@@ -143,7 +143,15 @@ int main(int argc, char* argv[]) {
 
 	//std::vector<std::pair<int, int>> list_position_goomba;
 	int nb_deplacement = 2500;
-	std::map<std::pair<int, int>, int> nb_pos;
+	std::map<std::pair<int, int>, int> nb_pos_goomba;
+	std::map<std::pair<int, int>, int> nb_pos_koopa;
+	std::map<std::pair<int, int>, int> nb_pos_flying_koopa;
+	std::map<std::pair<int, int>, int> nb_pos_piranha_plant;
+	std::map<std::pair<int, int>, int> nb_pos_hammer_bro;
+	std::map<std::pair<int, int>, int> nb_pos_bowser;
+	std::map<std::pair<int, int>, int> nb_pos_flying_fish;
+	std::map<std::pair<int, int>, int> nb_pos_lakitu;
+	std::map<std::pair<int, int>, int> nb_pos_turtle_spike;
 
 	for (int i = 0; i < nb_deplacement; i++) {
 
@@ -152,11 +160,11 @@ int main(int argc, char* argv[]) {
 		// On traite d'abord les goombas (Marron -> Orange sur l'image) (Koopa -> Vert / Plante piranha -> Rouge / Frère Marteau -> Bleu / Lakitu -> Blanc / Bowser -> Blanc / Poisson volant -> / Turtle Spike -> Gris)
 
 		for (enemy &ene : list_enemy_goomba) {
-			if (nb_pos.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos.end()) {
-				nb_pos.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
+			if (nb_pos_goomba.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_goomba.end()) {
+				nb_pos_goomba.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
 			}
 			else {
-				nb_pos[std::pair<int, int>(ene.x, ene.y)] += 1;
+				nb_pos_goomba[std::pair<int, int>(ene.x, ene.y)] += 1;
 			}
 			// Si le goomba se déplace vers la gauche
 			if (ene.isWalkingLeft) {
@@ -208,14 +216,79 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
-		std::map<std::pair<int, int>, int>::iterator it;
-		for (it = nb_pos.begin(); it != nb_pos.end(); it++) {
-			cv::Rect rect(it->first.second, it->first.first, 16, 16);
-			cv::rectangle(Image_Final, rect,
-				cv::Scalar(0, 29+it->second*2, 58 + it->second * 5),
-				cv::FILLED); //BGR
+		for (enemy& ene : list_enemy_koopa) {
+			if (nb_pos_koopa.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_koopa.end()) {
+				nb_pos_koopa.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
+			}
+			else {
+				nb_pos_koopa[std::pair<int, int>(ene.x, ene.y)] += 1;
+			}
+			// Si le koopa se déplace vers la gauche
+			if (ene.isWalkingLeft) {
 
+				// On check d'abord si y a un tuyau qui l'empêche d'avancer
+
+				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y - 1)) != list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y += 1;
+				}
+				else {
+					ene.y -= 1;
+				}
+
+				// On check maintenant si y a pas un trou sous lui 
+
+				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y)) != list_pixel.end()) {
+					ene.y -= 1;
+				}
+				else {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y += 1;
+				}
+
+			}
+			// S'il se déplace vers la droite 
+			else {
+
+				// On check d'abord si y a un tuyau qui l'empêche d'avancer
+
+				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y + 1)) != list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y -= 1;
+				}
+				else {
+					ene.y += 1;
+				}
+
+				// On check maintenant si y a pas un trou sous lui
+
+				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 16)) != list_pixel.end()) {
+					ene.y += 1;
+				}
+				else {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y -= 1;
+				}
+
+			}
 		}
+
+
+		std::map<std::pair<int, int>, int>::iterator it_goomba;
+		for (it_goomba = nb_pos_goomba.begin(); it_goomba != nb_pos_goomba.end(); it_goomba++) {
+			cv::Rect rect(it_goomba->first.second, it_goomba->first.first, 16, 16);
+			cv::rectangle(Image_Final, rect,
+				cv::Scalar(0, 29+it_goomba->second*2, 58 + it_goomba->second * 2),
+				cv::FILLED); //BGR
+		}
+		std::map<std::pair<int, int>, int>::iterator it_koopa;
+		for (it_koopa = nb_pos_koopa.begin(); it_koopa != nb_pos_koopa.end(); it_koopa++) {
+			cv::Rect rect(it_koopa->first.second, it_koopa->first.first, 16, 16);
+			cv::rectangle(Image_Final, rect,
+				cv::Scalar(0, 20 + it_koopa->second * 3, 0),
+				cv::FILLED); //BGR
+		}
+
 		std::string test = std::to_string(i);
 		std::string filename = "\\move_goomba_" + test + ".png";
 		cv::imwrite(base_path + level_path + "\\Images_move_goomba" + filename, Image_Final);
