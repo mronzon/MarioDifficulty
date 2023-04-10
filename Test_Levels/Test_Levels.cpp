588bdf63c3487bf6ab432a6c9c2666b33ce7123a
mronzon <104356392+mronzon@users.noreply.github.com>
2023-04-10 01:00:05 -0400

    Create argument parser

--- Test_Levels/Test_Levels.cpp
+++ Test_Levels/Test_Levels.cpp
@@ -7,15 +7,46 @@
 
 using namespace std;
 
+bool convert_string(string const& str)
+{
+    if(str == "false")
+    {
+        return false;
+    }
+    return true;
+}
+
 int main(int argc, char* argv[])
 {
-    if(argc != 6)
+    if(argc != 8)
         return -1;
     string const base_path = argv[1];
     string level_path;
     string folder_path;
     size_t result;
-    bool const create_all_jsons = false;
+    bool create_all_jsons = false;
+    bool create_dangers = false;
+    bool create_reachs = false;
+    for (int i = 2; i < argc - 1; i++)
+    {
+        string to_switch = argv[i];
+        if(to_switch == "--json")
+        {
+            create_all_jsons = convert_string(argv[i+1]);
+            i++;
+        }
+        if(to_switch == "--danger")
+        {
+            create_dangers = convert_string(argv[i+1]);
+            i++;
+        }
+        if(to_switch == "--reach")
+        {
+            create_reachs = convert_string(argv[i+1]);
+            i++;
+        }
+    }
+    
     for(auto const& entry : filesystem::directory_iterator(base_path))
     {
         folder_path = entry.path().string();
@@ -34,8 +65,12 @@ int main(int argc, char* argv[])
                 cout << "Probleme avec ce niveau" << endl;
             }
         }
-        cout << "Creation du danger pour " << level_path << endl;
-        create_danger(folder_path);
+        if(create_dangers)
+        {
+            cout << "Creation du danger pour " << level_path << endl;
+            create_danger(folder_path);
+        }
+        
         
         
         
