d65cf3bd52ed577b0807c81be43bcfebc5f11f1d
JulesLinard <79532003+JulesLinard@users.noreply.github.com>
2023-04-11 06:37:40 +0200

    Modif Koopa/Goomba + Rajout Tortue

--- Enemy_Data/testgoomba.cpp
+++ Enemy_Data/testgoomba.cpp
@@ -55,6 +55,7 @@ int main(int argc, char* argv[]) {
 	std::vector<enemy> list_enemy_flying_fish;
 	std::vector<enemy> list_enemy_lakitu;
 	std::vector<enemy> list_enemy_turtle_spike;
+	std::vector<enemy> list_enemy_turtle;
 
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "goomba") {
@@ -110,6 +111,12 @@ int main(int argc, char* argv[]) {
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
+		if (j["dynamic"]["enemies"][i]["type"] == "turtle") {
+			list_enemy_turtle.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
+				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
+		}
+	}
 
 	// On lit et on stocke la liste des paires de pixel qui composent les collisions
 
@@ -144,7 +151,7 @@ int main(int argc, char* argv[]) {
 	// Maintenant qu'on a la position des goombas et des collisions, on peut g�rer le d�placement des goombas
 
 	//std::vector<std::pair<int, int>> list_position_goomba;
-	int nb_deplacement = 2500;
+	int nb_deplacement = 1000;
 	std::map<std::pair<int, int>, int> nb_pos_goomba;
 	std::map<std::pair<int, int>, int> nb_pos_koopa;
 	std::map<std::pair<int, int>, int> nb_pos_flying_koopa;
@@ -154,6 +161,7 @@ int main(int argc, char* argv[]) {
 	std::map<std::pair<int, int>, int> nb_pos_flying_fish;
 	std::map<std::pair<int, int>, int> nb_pos_lakitu;
 	std::map<std::pair<int, int>, int> nb_pos_turtle_spike;
+	std::map<std::pair<int, int>, int> nb_pos_turtle; 
 
 	for (int i = 0; i < nb_deplacement; i++) {
 
@@ -171,23 +179,23 @@ int main(int argc, char* argv[]) {
 			// Si le goomba se d�place vers la gauche
 			if (ene.isWalkingLeft) {
 
-				// On check d'abord si y a un tuyau qui l'emp�che d'avancer
+				// On check d'abord si y a un tuyau qui l'emp�che d'avancer auquel cas on fait demi tour
 
 				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y - 1)) != list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y += 1; 
 				}
-				else {
-					ene.y -= 1;
-				}
 
-				// On check maintenant si y a pas un trou sous lui 
+				// On check maintenant si y a pas un trou sous lui auquel cas il tombe
 
-				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 17, ene.y + 16)) != list_pixel.end()) {
-					ene.y -= 1;
-				}
-				else {
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 17, ene.y + 16)) == list_pixel.end()) {
 					ene.x += 5;
+					ene.y -= 1;
+				}
+
+				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
+
+				else {
 					ene.y -= 1;
 				}
 
@@ -201,17 +209,17 @@ int main(int argc, char* argv[]) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y -= 1;
 				}
-				else {
-					ene.y += 1;
-				}
 
 				// On check maintenant si y a pas un trou sous lui
 
-				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 17, ene.y)) != list_pixel.end()) {
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 17, ene.y)) == list_pixel.end()) {
+					ene.x += 5;
 					ene.y += 1;
 				}
+
+				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
+
 				else {
-					ene.x += 5;
 					ene.y += 1;
 				}
 
@@ -234,18 +242,25 @@ int main(int argc, char* argv[]) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y += 1;
 				}
-				else {
+
+				// On check maintenant si y a pas un trou sous lui auquel cas il tombe (cas où l'ennemi est au dessus du vide au début)
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y)) == list_pixel.end()) {
+					ene.x += 5;
 					ene.y -= 1;
 				}
 
-				// On check maintenant si y a pas un trou sous lui 
+				// On check si le prochain pas va l'emmener au dessus du vide, auquel cas il faudra faire demi-tour
 
-				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y)) != list_pixel.end()) {
-					ene.y -= 1;
-				}
-				else {
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y-1)) == list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y += 1;
+				}
+				
+				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
+
+				else {
+					ene.y -= 1;
 				}
 
 			}
@@ -258,18 +273,167 @@ int main(int argc, char* argv[]) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y -= 1;
 				}
-				else {
+
+				// On check maintenant si y a pas un trou sous lui auquel cas il tombe (cas où l'ennemi est au dessus du vide au début)
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 16)) == list_pixel.end()) {
 					ene.y += 1;
+					ene.x += 5;
 				}
 
-				// On check maintenant si y a pas un trou sous lui
+				// On check si le prochain pas va l'emmener au dessus du vide, auquel cas on fait demi-tour
 
-				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 16)) != list_pixel.end()) {
-					ene.y += 1;
-				}
-				else {
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 17)) == list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y -= 1;
+				}
+
+				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
+
+				else {
+					ene.y += 1;
+				}
+
+			}
+		}
+
+		for (enemy& ene : list_enemy_turtle_spike) {
+			if (nb_pos_turtle_spike.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_turtle_spike.end()) {
+				nb_pos_turtle_spike.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
+			}
+			else {
+				nb_pos_turtle_spike[std::pair<int, int>(ene.x, ene.y)] += 1;
+			}
+			// Si le koopa se d�place vers la gauche
+			if (ene.isWalkingLeft) {
+
+				// On check d'abord si y a un tuyau qui l'emp�che d'avancer
+
+				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y - 1)) != list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y += 1;
+				}
+
+				// On check maintenant si y a pas un trou sous lui auquel cas il tombe (cas où l'ennemi est au dessus du vide au début)
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y)) == list_pixel.end()) {
+					ene.x += 5;
+					ene.y -= 1;
+				}
+
+				// On check si le prochain pas va l'emmener au dessus du vide, auquel cas il faudra faire demi-tour
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y - 1)) == list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y += 1;
+				}
+
+				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
+
+				else {
+					ene.y -= 1;
+				}
+
+			}
+			// S'il se d�place vers la droite 
+			else {
+
+				// On check d'abord si y a un tuyau qui l'emp�che d'avancer
+
+				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y + 1)) != list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y -= 1;
+				}
+
+				// On check maintenant si y a pas un trou sous lui auquel cas il tombe (cas où l'ennemi est au dessus du vide au début)
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 16)) == list_pixel.end()) {
+					ene.y += 1;
+					ene.x += 5;
+				}
+
+				// On check si le prochain pas va l'emmener au dessus du vide, auquel cas on fait demi-tour
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 17)) == list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y -= 1;
+				}
+
+				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
+
+				else {
+					ene.y += 1;
+				}
+
+			}
+		}
+
+		for (enemy& ene : list_enemy_turtle) {
+			if (nb_pos_turtle.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_turtle.end()) {
+				nb_pos_turtle.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
+			}
+			else {
+				nb_pos_turtle[std::pair<int, int>(ene.x, ene.y)] += 1;
+			}
+			// Si le koopa se d�place vers la gauche
+			if (ene.isWalkingLeft) {
+
+				// On check d'abord si y a un tuyau qui l'emp�che d'avancer
+
+				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y - 1)) != list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y += 1;
+				}
+
+				// On check maintenant si y a pas un trou sous lui auquel cas il tombe (cas où l'ennemi est au dessus du vide au début)
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y)) == list_pixel.end()) {
+					ene.x += 5;
+					ene.y -= 1;
+				}
+
+				// On check si le prochain pas va l'emmener au dessus du vide, auquel cas il faudra faire demi-tour
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y - 1)) == list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y += 1;
+				}
+
+				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
+
+				else {
+					ene.y -= 1;
+				}
+
+			}
+			// S'il se d�place vers la droite 
+			else {
+
+				// On check d'abord si y a un tuyau qui l'emp�che d'avancer
+
+				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y + 1)) != list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y -= 1;
+				}
+
+				// On check maintenant si y a pas un trou sous lui auquel cas il tombe (cas où l'ennemi est au dessus du vide au début)
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 16)) == list_pixel.end()) {
+					ene.y += 1;
+					ene.x += 5;
+				}
+
+				// On check si le prochain pas va l'emmener au dessus du vide, auquel cas on fait demi-tour
+
+				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 17)) == list_pixel.end()) {
+					ene.isWalkingLeft = !ene.isWalkingLeft;
+					ene.y -= 1;
+				}
+
+				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
+
+				else {
+					ene.y += 1;
 				}
 
 			}
@@ -283,6 +447,24 @@ int main(int argc, char* argv[]) {
 				cv::Scalar(0, 29+it_goomba->second*2, 58 + it_goomba->second * 2),
 				cv::FILLED); //BGR
 		}
+
+		std::map<std::pair<int, int>, int>::iterator it_turtle_spike;
+		for (it_turtle_spike = nb_pos_turtle_spike.begin(); it_turtle_spike != nb_pos_turtle_spike.end(); it_turtle_spike++) {
+			cv::Rect rect(it_turtle_spike->first.second, it_turtle_spike->first.first, 16, 16);
+			cv::rectangle(Image_Final, rect,
+				cv::Scalar(0, 0, 40 + it_turtle_spike->second * 2),
+				cv::FILLED); //BGR
+		}
+
+		std::map<std::pair<int, int>, int>::iterator it_turtle;
+		for (it_turtle = nb_pos_turtle.begin(); it_turtle != nb_pos_turtle.end(); it_turtle++) {
+			cv::Rect rect(it_turtle->first.second, it_turtle->first.first, 16, 16);
+			cv::rectangle(Image_Final, rect,
+				cv::Scalar(20 + it_turtle->second * 2, 20 + it_turtle->second * 2, 20 + it_turtle->second * 2),
+				cv::FILLED); //BGR
+		}
+
+
 		std::map<std::pair<int, int>, int>::iterator it_koopa;
 		for (it_koopa = nb_pos_koopa.begin(); it_koopa != nb_pos_koopa.end(); it_koopa++) {
 			cv::Rect rect(it_koopa->first.second, it_koopa->first.first, 23, 16);
@@ -291,9 +473,9 @@ int main(int argc, char* argv[]) {
 				cv::FILLED); //BGR
 		}
 
-		std::string test = std::to_string(i);
-		std::string filename = "\\move_goomba_" + test + ".png";
-		cv::imwrite(base_path + level_path + "\\Images_move_goomba" + filename, Image_Final);
+		std::string num_fichier = std::to_string(i);
+		std::string filename = "\\move_enemies" + num_fichier + ".png";
+		cv::imwrite(base_path + level_path + "\\Images_move_enemies" + filename, Image_Final);
 	}
 
 	// Calcul m�trique
@@ -304,31 +486,46 @@ int main(int argc, char* argv[]) {
 	float metric_global = 0.0f;
 	points_array points_goomba;
 	points_array points_koopa;
+	points_array points_turtle_spike;
+	points_array points_turtle;
 	points_array points_globaux;
 	
-	std::cout << "coucou" << std::endl;
 
 	for (int end_y = window_width; end_y < reach_filled_image.cols; end_y += step_y)
 	{
 		metric_global = 0;
-		metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_goomba, 216);
+		metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_goomba, 16);
 		metric_global += metric;
 		points_goomba.emplace_back(point(end_y, metric));
 
-		metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_koopa, 368);
+		metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_koopa, 23);
 		metric_global += metric;
 		points_koopa.emplace_back(point(end_y, metric));
+
+		metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_turtle_spike, 15);
+		metric_global += metric;
+		points_turtle_spike.emplace_back(point(end_y, metric));
+
+		metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_turtle, 15);
+		metric_global += metric;
+		points_turtle.emplace_back(point(end_y, metric));
 
 		points_globaux.emplace_back(point(end_y, metric_global));
 	}
 	create_graph(points_goomba, metric_path + "\\metric_goomba.png");
 	create_graph(points_koopa, metric_path + "\\metric_koopa.png");
+	create_graph(points_turtle_spike, metric_path + "\\metric_turtle_spike.png");
+	create_graph(points_turtle, metric_path + "\\metric_turtle.png");
 	std::vector<points_array> tab;
 	std::vector<cv::Scalar> colors;
 	tab.emplace_back(points_goomba);
-	colors.emplace_back(cv::Scalar(255,0,0));
+	colors.emplace_back(cv::Scalar(0,75,150));
 	tab.emplace_back(points_koopa);
-	colors.emplace_back(cv::Scalar(0,255,0));
+	colors.emplace_back(cv::Scalar(34,139,34));
+	tab.emplace_back(points_turtle_spike);
+	colors.emplace_back(cv::Scalar(0, 0, 255));
+	tab.emplace_back(points_turtle);
+	colors.emplace_back(cv::Scalar(100, 100, 100));
 	tab.emplace_back(points_globaux);
 	colors.emplace_back(cv::Scalar(255,255,255));
 	create_graph(tab, colors, metric_path + "\\metric_enemy_global.png");
