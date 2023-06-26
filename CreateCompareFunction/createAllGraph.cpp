7e69e8d7228d19cf7b520b99920be2f00134b313
JulesLinard <79532003+JulesLinard@users.noreply.github.com>
2023-06-26 05:22:22 +0200

    push qui sert à rien atm

--- CreateCompareFunction/createAllGraph.cpp
+++ CreateCompareFunction/createAllGraph.cpp
@@ -1,4 +1,5 @@
 ﻿#include "general.h"
+#include <map>
 
 float inline metric_area_filled(const cv::Mat& reach_filled, const cv::Mat& danger_filled, int start_y, int end_y) {
     int reach_count = 0;
@@ -8,6 +9,22 @@ float inline metric_area_filled(const cv::Mat& reach_filled, const cv::Mat& dang
         for (int y = start_y; y < end_y; y++) {
             reach_count += (reach_filled.at<uchar>(x, y) != 0);
             effective_count += (reach_filled.at<uchar>(x, y) && danger_filled.at<uchar>(x, y));
+        }
+
+    return static_cast<float>(effective_count) / static_cast<float>(reach_count);
+}
+
+float inline metric_enemy(const cv::Mat& reach_filled, int start_y, int end_y, std::map<std::pair<int, int>, int>& enemy_map, float surface) {
+    int reach_count = 0;
+    int effective_count = 0;
+
+    for (int x = 0; x < reach_filled.rows; x++)
+        for (int y = start_y; y < end_y; y++) {
+            reach_count += (reach_filled.at<uchar>(x, y) != 0);
+            // si le pixel est dans reach + qu'il y a des pixels dessus
+            if (reach_filled.at<uchar>(x, y) && enemy_map.find(std::pair<int, int>(x, y)) != enemy_map.end()) {
+                effective_count += enemy_map[std::pair<int, int>(x, y)] * surface;
+            }
         }
 
     return static_cast<float>(effective_count) / static_cast<float>(reach_count);
