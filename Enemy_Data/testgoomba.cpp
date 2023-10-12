ed4b52a35b43317b487dfdbfb9f9ba0545b9b8b6
MaximeLepageUQAC <maxime-lepage@hotmail.ca>
2023-10-12 18:23:40 -0400

    Début logique Lakitu

--- Enemy_Data/testgoomba.cpp
+++ Enemy_Data/testgoomba.cpp
@@ -46,60 +46,70 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 	std::vector<enemy> list_enemy_turtle;
 
 
+	// Créer la liste des Goomba présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "goomba") {
 			list_enemy_goomba.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(), 
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>()});
 		}
 	}
+	// Créer la liste des Koopa présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "koopa") {
 			list_enemy_koopa.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	// Créer la liste des Flying Koopa présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "flying_koopa") {
 			list_enemy_flying_koopa.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	// Créer la liste des Plante Piranha présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "piranha_plant") {
 			list_enemy_piranha_plant.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	// Créer la liste des Hammer Bro présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "hammer_bro") {
 			list_enemy_hammer_bro.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	// Créer la liste des Bowser présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "bowser") {
 			list_enemy_bowser.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	// Créer la liste des Flying Fish présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "flying_fish") {
 			list_enemy_flying_fish.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	// Créer la liste des Lakitu présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "lakitu") {
 			list_enemy_lakitu.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	// Créer la liste des Turtle Spike présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "turtle_spike") {
 			list_enemy_turtle_spike.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
 				j["dynamic"]["enemies"][i]["height"].as<int>(), j["dynamic"]["enemies"][i]["width"].as<int>(), j["dynamic"]["enemies"][i]["type"].as<std::string>() });
 		}
 	}
+	// Créer la liste des Turtle présent dans le niveau
 	for (int i = 0; i < j["dynamic"]["enemies"].size(); i++) {
 		if (j["dynamic"]["enemies"][i]["type"] == "turtle") {
 			list_enemy_turtle.push_back(enemy{ j["dynamic"]["enemies"][i]["x"].as<int>(), j["dynamic"]["enemies"][i]["y"].as<int>(),
@@ -109,7 +119,6 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 
 
 	// On lit et on stocke la liste des paires de pixel qui composent les collisions
-
 	std::vector<std::pair<int, int>> list_pixel;
 
 	for (int i = 0; i < j["static"]["pipes"].size(); i++) {
@@ -138,7 +147,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 		}
 	}
 
-	// Maintenant qu'on a la position des goombas et des collisions, on peut g�rer le d�placement des goombas
+	// Maintenant qu'on a la position des goombas et des collisions, on peut gerer le deplacement des ennemis
 
 	//std::vector<std::pair<int, int>> list_position_goomba;
 	int nb_deplacement = 250;
@@ -162,7 +171,9 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 
 		cv::Mat Image_Final(level_image.rows, level_image.cols, level_image.type(), cv::Scalar(0, 0, 0));
 		
-		// On traite d'abord les goombas (Marron -> Orange sur l'image) (Koopa -> Vert / Plante piranha -> Rouge / Fr�re Marteau -> Bleu / Lakitu -> Blanc / Bowser -> Blanc / Poisson volant -> / Turtle Spike -> Gris)
+		// Goombas (Marron -> Orange sur l'image) (Koopa -> Vert / Plante piranha -> Rouge / Frère Marteau -> Bleu / Lakitu -> Blanc / Bowser -> Blanc / Poisson volant -> / Turtle Spike -> Gris)
+		
+		// On simule le déplacement pour les Gooma
 		for (enemy &ene : list_enemy_goomba) {
 			if (nb_pos_goomba.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_goomba.end()) {
 				nb_pos_goomba.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
@@ -173,10 +184,10 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 
 				nb_pos_goomba[std::pair<int, int>(ene.x, ene.y)] += 1;
 			}
-			// Si le goomba se d�place vers la gauche
+			// Si le goomba se deplace vers la gauche
 			if (ene.isWalkingLeft) {
 
-				// On check d'abord si y a un tuyau qui l'emp�che d'avancer auquel cas on fait demi tour
+				// On check d'abord si y a un tuyau qui l'empêche d'avancer auquel cas on fait demi tour
 
 				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y - 1)) != list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
@@ -197,10 +208,10 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 				}
 
 			}
-			// S'il se d�place vers la droite 
+			// S'il se deplace vers la droite 
 			else {
 
-				// On check d'abord si y a un tuyau qui l'emp�che d'avancer
+				// On check d'abord si y a un tuyau qui l'empêche d'avancer
 
 				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y + 1)) != list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
@@ -222,6 +233,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 
 			}
 		}
+		// On simule le déplacement pour les Koopa
 		for (enemy& ene : list_enemy_koopa) {
 			if (nb_pos_koopa.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_koopa.end()) {
 				nb_pos_koopa.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
@@ -295,6 +307,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 
 			}
 		}
+		// On simule le déplacement pour les Turtle Spike
 		for (enemy& ene : list_enemy_turtle_spike) {
 			if (nb_pos_turtle_spike.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_turtle_spike.end()) {
 				nb_pos_turtle_spike.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
@@ -333,7 +346,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 				}
 
 			}
-			// S'il se d�place vers la droite 
+			// S'il se déplace vers la droite 
 			else {
 
 				// On check d'abord si y a un tuyau qui l'emp�che d'avancer
@@ -365,6 +378,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 
 			}
 		}
+		// On simule le déplacement pour les Turtle
 		for (enemy& ene : list_enemy_turtle) {
 			if (nb_pos_turtle.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_turtle.end()) {
 				nb_pos_turtle.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
@@ -403,7 +417,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 				}
 
 			}
-			// S'il se d�place vers la droite 
+			// S'il se déplace vers la droite 
 			else {
 
 				// On check d'abord si y a un tuyau qui l'emp�che d'avancer
@@ -435,6 +449,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 
 			}
 		}
+		// On simule le déplacement pour les Plante Piranha
 		for (enemy& ene : list_enemy_piranha_plant) {
 			if (nb_pos_piranha_plant.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_piranha_plant.end()) {
 				nb_pos_piranha_plant.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
@@ -457,7 +472,23 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 				ene.isWalkingLeft = !ene.isWalkingLeft;			
 			}			
 		}
+		// On simule le déplacement pour les Lakitu
 		for (enemy& ene : list_enemy_lakitu) {
+
+			int higherObstacleY = 0;
+			for (std::pair<int, int>& obstacle : list_pixel)
+			{
+				/*
+				 si la pos x de l'ennemi est plus grand ou égale à la pos de l'obstacle en x et plus petit que la pos x de l'obstacle plus sa largeur. (S'il est au dessus de l'obstacle)
+				 if (ene.x >= obstacle.first && ene.x <= obstacle.first)
+				*/
+				// si la pos y de l'obstacle est plus grand que higherObstacleY, alors higherObstacleY sera égale à se Y pour placer les phéromones seulement sur l'obstacle le plus haut, pas tous les obstacles en dessous.
+				if (obstacle.second > higherObstacleY)
+				{
+					higherObstacleY = obstacle.second;
+				}
+			}
+
 			if (nb_pos_lakitu.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_lakitu.end()) {
 				nb_pos_lakitu.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
 			}
@@ -480,6 +511,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 
 			}
 		}
+		// On simule le déplacement pour les Hammer Bro
 		for (enemy& ene : list_enemy_hammer_bro) {
 			if (nb_pos_hammer_bro.find(std::pair<int, int>(ene.x, ene.y)) == nb_pos_hammer_bro.end()) {
 				nb_pos_hammer_bro.insert({ std::pair<int, int>(ene.x, ene.y), 1 });
@@ -491,28 +523,24 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 			if (ene.isWalkingLeft) {
 
 				// On check d'abord si y a un tuyau qui l'empeche d'avancer
-
 				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y - 1)) != list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y += 1;
 				}
 
 				// On check maintenant si y a pas un trou sous lui auquel cas il tombe (cas où l'ennemi est au dessus du vide au début)
-
 				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y)) == list_pixel.end()) {
 					ene.x += 5;
 					ene.y -= 1;
 				}
 
 				// On check si le prochain pas va l'emmener au dessus du vide, auquel cas il faudra faire demi-tour
-
 				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y - 1)) == list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y += 1;
 				}
 
 				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
-
 				else {
 					ene.y -= 1;
 				}
@@ -522,28 +550,24 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 			else {
 
 				// On check d'abord si y a un tuyau qui l'empeche d'avancer
-
 				if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x, ene.y + 1)) != list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y -= 1;
 				}
 
 				// On check maintenant si y a pas un trou sous lui auquel cas il tombe (cas où l'ennemi est au dessus du vide au début)
-
 				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 16)) == list_pixel.end()) {
 					ene.y += 1;
 					ene.x += 5;
 				}
 
 				// On check si le prochain pas va l'emmener au dessus du vide, auquel cas on fait demi-tour
-
 				else if (std::find(list_pixel.begin(), list_pixel.end(), std::pair<int, int>(ene.x + 24, ene.y + 17)) == list_pixel.end()) {
 					ene.isWalkingLeft = !ene.isWalkingLeft;
 					ene.y -= 1;
 				}
 
 				// Si aucun de ces cas ne se réalisent, alors ça signifie qu'on peut avancer tranquillement
-
 				else {
 					ene.y += 1;
 				}
@@ -562,6 +586,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 		}
 		
 		if (create_images) {
+			// On crée l'image avec les phéromones pour les Goomba
 			std::map<std::pair<int, int>, int>::iterator it_goomba;
 			for (it_goomba = nb_pos_goomba.begin(); it_goomba != nb_pos_goomba.end(); it_goomba++) {
 				cv::Rect rect(it_goomba->first.second, it_goomba->first.first, 16, 16);
@@ -570,6 +595,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 					cv::FILLED); //BGR
 			}
 
+			// On crée l'image avec les phéromones pour les Turtle Spike
 			std::map<std::pair<int, int>, int>::iterator it_turtle_spike;
 			for (it_turtle_spike = nb_pos_turtle_spike.begin(); it_turtle_spike != nb_pos_turtle_spike.end(); it_turtle_spike++) {
 				cv::Rect rect(it_turtle_spike->first.second, it_turtle_spike->first.first, 15, 16);
@@ -578,6 +604,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 					cv::FILLED); //BGR
 			}
 
+			// On crée l'image avec les phéromones pour les Turtle
 			std::map<std::pair<int, int>, int>::iterator it_turtle;
 			for (it_turtle = nb_pos_turtle.begin(); it_turtle != nb_pos_turtle.end(); it_turtle++) {
 				cv::Rect rect(it_turtle->first.second, it_turtle->first.first, 15, 16);
@@ -586,7 +613,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 					cv::FILLED); //BGR
 			}
 
-
+			// On crée l'image avec les phéromones pour les Koopa
 			std::map<std::pair<int, int>, int>::iterator it_koopa;
 			for (it_koopa = nb_pos_koopa.begin(); it_koopa != nb_pos_koopa.end(); it_koopa++) {
 				cv::Rect rect(it_koopa->first.second, it_koopa->first.first, 23, 16);
@@ -595,6 +622,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 					cv::FILLED); //BGR
 			}
 
+			// On crée l'image avec les phéromones pour les Plante Piranha
 			std::map<std::pair<int, int>, int>::iterator it_piranha_plant;
 			for (it_piranha_plant = nb_pos_piranha_plant.begin(); it_piranha_plant != nb_pos_piranha_plant.end(); it_piranha_plant++) {
 				cv::Rect rect(it_piranha_plant->first.second, it_piranha_plant->first.first, 22, 16);
@@ -610,6 +638,8 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 	}
 
 	cv::Mat Image_Final_Temp(level_image.rows, level_image.cols, level_image.type(), cv::Scalar(0, 0, 0));
+
+	// On crée les phéromones pour les Goomba
 	std::map<std::pair<int, int>, int>::iterator it_goomba;
 	for (it_goomba = nb_pos_goomba.begin(); it_goomba != nb_pos_goomba.end(); it_goomba++) {
 		cv::Rect rect(it_goomba->first.second, it_goomba->first.first, 16, 16);
@@ -618,6 +648,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 			cv::FILLED); //BGR
 	}
 
+	// On crée les phéromones pour les Turtle Spike
 	std::map<std::pair<int, int>, int>::iterator it_turtle_spike;
 	for (it_turtle_spike = nb_pos_turtle_spike.begin(); it_turtle_spike != nb_pos_turtle_spike.end(); it_turtle_spike++) {
 		cv::Rect rect(it_turtle_spike->first.second, it_turtle_spike->first.first, 15, 16);
@@ -626,6 +657,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 			cv::FILLED); //BGR
 	}
 
+	// On crée les phéromones pour les Turtle
 	std::map<std::pair<int, int>, int>::iterator it_turtle;
 	for (it_turtle = nb_pos_turtle.begin(); it_turtle != nb_pos_turtle.end(); it_turtle++) {
 		cv::Rect rect(it_turtle->first.second, it_turtle->first.first, 15, 16);
@@ -634,7 +666,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 			cv::FILLED); //BGR
 	}
 
-
+	// On crée les phéromones pour les Koopa
 	std::map<std::pair<int, int>, int>::iterator it_koopa;
 	for (it_koopa = nb_pos_koopa.begin(); it_koopa != nb_pos_koopa.end(); it_koopa++) {
 		cv::Rect rect(it_koopa->first.second, it_koopa->first.first, 23, 16);
@@ -643,6 +675,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 			cv::FILLED); //BGR
 	}
 
+	// On crée les phéromones pour les Plante Piranha
 	std::map<std::pair<int, int>, int>::iterator it_piranha_plant;
 	for (it_piranha_plant = nb_pos_piranha_plant.begin(); it_piranha_plant != nb_pos_piranha_plant.end(); it_piranha_plant++) {
 		cv::Rect rect(it_piranha_plant->first.second, it_piranha_plant->first.first, 22, 16);
@@ -655,7 +688,7 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 	std::string filename = "\\move_enemies" + num_fichier + ".png";
 	cv::imwrite(base_path + level_path + "\\Images_move_enemies" + filename, Image_Final_Temp);
 
-	// Calcul m�trique
+	// Ici c'est la section du Calcul metrique
 
 	int window_width = 16;
 	int step_y = 16;
@@ -673,23 +706,28 @@ void create_metric(std::string base_path, std::string level_path, bool create_im
 	for (int window_width = 16; window_width <= 320; window_width += 16) {
 		for (int end_y = window_width; end_y < reach_filled_image.cols; end_y += step_y)
 		{
+			// Création de la métrique pour les Goomba
 			metric_global = 0;
 			metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_goomba, 16);
 			metric_global += metric;
 			points_goomba.emplace_back(point(end_y, metric));
 
+			// Création de la métrique pour les Koopa
 			metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_koopa, 23);
 			metric_global += metric;
 			points_koopa.emplace_back(point(end_y, metric));
 
+			// Création de la métrique pour les Turtle Spike
 			metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_turtle_spike, 15);
 			metric_global += metric;
 			points_turtle_spike.emplace_back(point(end_y, metric));
 
+			// Création de la métrique pour les Turtle
 			metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_turtle, 15);
 			metric_global += metric;
 			points_turtle.emplace_back(point(end_y, metric));
 
+			// Création de la métrique pour les Plante Piranha
 			metric = metric_area_filled(reach_filled_image, end_y - window_width, end_y, nb_pos_piranha_plant, 22);
 			metric_global += metric;
 			points_piranha_plant.emplace_back(point(end_y, metric));
